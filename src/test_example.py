79df6b85fe85bcd79bc0c34158cbc80f27c1db12
Brian Sipos <brian.sipos@jhuapl.edu>
2026-07-29 16:53:10 -0400

    Add full-length tag example using KDF (#20)

--- src/test_example.py
+++ src/test_example.py
@@ -1,5 +1,6 @@
 import logging
 import unittest
+from typing import cast
 
 from cbor_diag import cbor2diag
 from pycose import headers
@@ -8,10 +9,12 @@ from pycose.algorithms import (
     AESCMAC128_128,
     AESCMAC256_96,
     AESCMAC256_128,
+    DirectHKDFSHA512,
 )
 from pycose.exceptions import CoseException
 from pycose.keys import SymmetricKey, keyops, keyparam
-from pycose.messages import CoseMessage, Mac0Message
+from pycose.messages import CoseMessage, Mac0Message, MacMessage
+from pycose.messages.recipient import DirectEncryption
 
 LOGGER = logging.getLogger(__name__)
 
@@ -59,6 +62,7 @@ class TestExample(unittest.TestCase):
                 # Verify from endoded form
                 msg_back = CoseMessage.decode(message_enc)
                 self.assertIsInstance(msg_back, Mac0Message)
+                msg_back = cast(Mac0Message, msg_back)
                 with self.assertRaises(CoseException):
                     msg_back.verify_tag()
                 msg_back.key = key
@@ -108,7 +112,70 @@ class TestExample(unittest.TestCase):
                 # Verify from endoded form
                 msg_back = CoseMessage.decode(message_enc)
                 self.assertIsInstance(msg_back, Mac0Message)
+                msg_back = cast(Mac0Message, msg_back)
                 with self.assertRaises(CoseException):
                     msg_back.verify_tag()
                 msg_back.key = key
                 msg_back.verify_tag()
+
+    def test_CMAC256_KDF(self):
+        kdk = SymmetricKey(
+            k=bytes.fromhex(
+                "5e1047c9b428ee26d2c1059737301756effbeec673f283cff992f132d6a3cb8a"
+            ),
+            optional_params={
+                keyparam.KpKid: b"ExampleA.2",
+                keyparam.KpAlg: DirectHKDFSHA512,
+                keyparam.KpKeyOps: [keyops.DeriveKeyOp],
+            },
+        )
+        LOGGER.info("KDK: %s", cbor2diag(kdk.encode()))
+
+        alg = AESCMAC256_128
+        msg_obj = MacMessage(
+            phdr={
+                headers.Algorithm: alg,
+            },
+            payload=b"This is the content.",
+            recipients=[
+                DirectEncryption(
+                    phdr={
+                        headers.Algorithm: kdk.alg,
+                    },
+                    uhdr={
+                        headers.KID: kdk.kid,
+                        headers.Salt: bytes.fromhex("673b4e76"),
+                    },
+                )
+            ],
+            # Non-encoded parameters
+            external_aad=b"",
+        )
+        self.assertIsInstance(msg_obj.recipients, list)
+        msg_obj.recipients[0].key = kdk
+
+        # COSE internal structure
+        cose_struct_enc = msg_obj._mac_structure
+        LOGGER.info("COSE Structure: %s", cbor2diag(cose_struct_enc))
+        LOGGER.info("Encoded: %s", cose_struct_enc.hex())
+
+        # Encoded message
+        message_enc = msg_obj.encode(tag=True)
+        LOGGER.info("Message: %s", cbor2diag(message_enc))
+        LOGGER.info("Encoded: %s", message_enc.hex())
+
+        kdf_context_enc = msg_obj.recipients[0].get_kdf_context(alg).encode()
+        LOGGER.info("KDF Context: %s", cbor2diag(kdf_context_enc))
+        LOGGER.info("Encoded: %s", kdf_context_enc.hex())
+        LOGGER.info("Content key: %s", cbor2diag(msg_obj.key.encode()))
+
+        # Verify from endoded form
+        msg_back = CoseMessage.decode(message_enc)
+        self.assertIsInstance(msg_back, MacMessage)
+        msg_back = cast(MacMessage, msg_back)
+        self.assertEqual(1, len(msg_back.recipients))
+        recip = msg_back.recipients[0]
+        with self.assertRaises(AttributeError):
+            msg_back.verify_tag(recip)
+        recip.key = kdk
+        msg_back.verify_tag(recip)
