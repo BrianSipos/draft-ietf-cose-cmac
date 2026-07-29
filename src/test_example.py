a04853b414cd26813b7672319a136ebd1b3e73d9
Brian Sipos <brian.sipos@jhuapl.edu>
2026-07-29 16:49:42 -0400

    format

--- src/test_example.py
+++ src/test_example.py
@@ -164,11 +164,7 @@ class TestExample(unittest.TestCase):
         LOGGER.info("Message: %s", cbor2diag(message_enc))
         LOGGER.info("Encoded: %s", message_enc.hex())
 
-        kdf_context_enc = (
-            msg_obj.recipients[0]
-            .get_kdf_context(alg)
-            .encode()
-        )
+        kdf_context_enc = msg_obj.recipients[0].get_kdf_context(alg).encode()
         LOGGER.info("KDF Context: %s", cbor2diag(kdf_context_enc))
         LOGGER.info("Encoded: %s", kdf_context_enc.hex())
         LOGGER.info("Content key: %s", cbor2diag(msg_obj.key.encode()))
